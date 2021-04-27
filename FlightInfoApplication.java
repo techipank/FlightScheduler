58ec0808305570bd76de133d3a200333b218d041
Pankaj Kumar <pankajjsr23@gmail.com>
2021-04-27 14:01:59 +0530

    Create FlightInfoApplication.java

---
+++ FlightInfoApplication.java
@@ -0,0 +1,13 @@
+package com.pankaj.flightschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInfoApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(FlightInfoApplication.class, args);
+}
+
+}
